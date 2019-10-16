dc4fcb4516d3cb16ab0a212413c0dc80d687784b
Afzal Mohammed <afzal.mohd.ma@gmail.com>
2019-10-16 12:03:31 +0530

    bl_dtb_append: calculate DTB_ADDR automatically
    Signed-off-by: Afzal Mohammed <afzal.mohd.ma@gmail.com>

--- bl_dtb_append.sh
+++ bl_dtb_append.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 DTB_FILE=${1:-stm32f429-disco.dtb}
-BOOTLOADER_FILE=${2:-stm32f429i-disco.bin}
+BOOTLOADER_FILE=stm32f429i-disco.bin
 OUTPUT_FILE=stm32f429i-disco.bindtb
 
 if [ ! -f ${DTB_FILE} ]; then
@@ -9,12 +9,17 @@ if [ ! -f ${DTB_FILE} ]; then
 	exit 1
 fi
 
-make ARCH=arm CROSS_COMPILE=arm-known-linux-gnueabihf- clean
-make ARCH=arm CROSS_COMPILE=arm-known-linux-gnueabihf- stm32f429i-disco DTB_ADDR=0x0800068e
+set -e
 
-if [ ! -f ${BOOTLOADER_FILE} ]; then
-	echo "bootloader file not present"
-	exit 1
-fi
+make ARCH=arm CROSS_COMPILE=arm-known-linux-gnueabihf- clean
+make ARCH=arm CROSS_COMPILE=arm-known-linux-gnueabihf- stm32f429i-disco
+
+BL_SIZE=`ls -l ${BOOTLOADER_FILE} | cut -d ' ' -f5`
+let "BL_SIZE = ${BL_SIZE} + 0x08000000"
+
+# echo $BL_SIZE
+
+make ARCH=arm CROSS_COMPILE=arm-known-linux-gnueabihf- clean
+make ARCH=arm CROSS_COMPILE=arm-known-linux-gnueabihf- stm32f429i-disco DTB_ADDR=${BL_SIZE}
 
 cat ${BOOTLOADER_FILE} ${DTB_FILE} > ${OUTPUT_FILE}
